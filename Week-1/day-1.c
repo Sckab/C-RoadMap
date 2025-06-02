18670028f0a419b98eac7882c1d2ed15ef3a9ce7
Sckab <giulianodeamicis2009@gmail.com>
2025-06-02 19:13:08 +0200

    made the first day of the road map!

---
+++ Week-1/day-1.c
@@ -0,0 +1,41 @@
+/*
+ *    This is the first day of my road map.
+ *    Day 1 of my roadmap: learning how to print stuff in C.
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    // Define a variable
+    int x = 10;
+
+    // Declare a variable
+    int y;
+
+    // Assign a value to the variable
+    y = 20;
+
+    // Define a character
+    char a = 'a';
+ 
+    // Print a string
+    printf("Hello World\n");
+
+    // Print a number
+    printf("%d\n", x);
+
+    // Print another number
+    printf("%d\n", y);
+
+    // Print a character
+    printf("%c\n", a);
+    
+    return 0;
+
+}
+
+/*
+ *    This was a pretty easy day.
+ *    I learnt the easiest things about C.
+ */
