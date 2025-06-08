0d39025f00a69dcc8c9353e8f1f2a8324b64c87e
Sckab <giulianodeamicis2009@gmail.com>
2025-06-08 19:18:34 +0200

    completed the first week!

---
+++ Week-1/mini-project/globals.c
@@ -0,0 +1,9 @@
+/*
+ *    This is the file that contains the global variables
+ */
+
+#include "globals.h"
+
+double num1;
+double num2;
+char operation;
---
+++ Week-1/mini-project/globals.h
@@ -0,0 +1,12 @@
+/*
+ *    This is the header file for the globals.c file
+ */
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+extern double num1;
+extern double num2;
+extern char operation;
+
+#endif
